b1dded3e1fa30f065c8ce08253eaea7350827998
ikram abdillah <ikram.koli@gmail.com>
2022-12-02 15:00:22 +0700

    Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
+import Image from "next/image";
+import pp from "../assets/ikram.JPEG";
+import Link from "next/link";
 
 type Props = {};
 
@@ -10,9 +14,31 @@ export default function Hero({}: Props) {
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <p>{text}</p>
-      <Cursor cursorColor="#F7AB0A" />
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <Image className="relative rounded-full h-32 w-32 mx-auto object-cover" src={pp} alt="Photo Profile" />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2  tracking-[10px]">Web Developer</h2>
+        <h1 className="text-4xl lg:text-5xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#03AC13" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#project">
+            <button className="heroButton">Project</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
